2bbdd7acaf66eb787df055c83eae422cc5eea495
PMIScoutAI <investimentolibero@gmail.com>
2025-08-04 13:05:01 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import '../styles/globals.css'
+// /pages/_app.js
+// File principale dell'applicazione - Modificato per usare il nuovo AuthProvider
 
+import { AuthProvider } from '../contexts/AuthContext'; // Importa il nuovo gestore
+import '../styles/globals.css';
+
+// Il nome della funzione può essere 'App' o 'MyApp', non fa differenza.
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    // Avvolgiamo tutta l'app con l'AuthProvider.
+    // In questo modo, ogni pagina avrà accesso allo stato di login in modo sicuro.
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
