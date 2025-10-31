9fb5f0b5d2a6462963d83e825e2c3eb76830dd50
PMIScoutAI <investimentolibero@gmail.com>
2025-10-31 09:08:36 +0100

    Enhance next.config.js with API and webpack settings
    Added custom API body parser size limit and modified webpack configuration for server externals.

--- next.config.js
+++ next.config.js
@@ -1,1 +1,20 @@
-module.exports = { reactStrictMode: true };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  api: {
+    bodyParser: {
+      sizeLimit: '50mb',
+    },
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push({
+        'sharp': 'sharp',
+        'pdfjs-dist': 'pdfjs-dist'
+      });
+    }
+    return config;
+  },
+};
+
+module.exports = nextConfig;
